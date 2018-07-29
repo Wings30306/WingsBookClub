f76731b81152855aa69188fa234f4542c096d8b2
Joke Heyndels <jo_hannah@outlook.com>
2018-07-29 22:05:39 +0200

    trying to fix forms

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,10 +1,15 @@
 function sendMail(contactForm) {
+  var contactForm = $("form")
+
+  console.log(contactForm)
   emailjs.send("gmail","book_club",{
     "from_name": contactForm.name.value,
     "from_email": contactForm.emailaddress.value,
     "title":contactForm.title.value,
     "message": contactForm.description.value,
   })
+  console.log(contactForm)
+
   .then(
     function(response) {
     console.log("SUCCESS", response);
